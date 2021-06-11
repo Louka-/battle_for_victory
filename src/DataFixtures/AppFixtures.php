070326e882b36a5d9a1cc2e75704365b48fad6ce
Louka- <simon.haudegond@gmail.com>
2021-06-11 10:09:04 +0200

    commit fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,9 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +12,24 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $admin = new User;
+        $pwdcrypted = $this->passwordEncoder->encodePassword($admin, '1234');
+        $admin
+            ->setEmail('admin@carte.fr')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($pwdcrypted);
+        
+        $manager->persist($admin);
+
+        $user = new User;
+        $pwdcrypted = $this->passwordEncoder->encodePassword($user, '1234');
+        $user
+            ->setEmail('user@carte.fr')
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($pwdcrypted);
+        
+        $manager->persist($user);
 
         $manager->flush();
     }
