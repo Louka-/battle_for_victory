ee49c2b11cefd34f9e605f0ea724a77ba2ade96e
Louka- <simon.haudegond@gmail.com>
2021-06-11 10:43:00 +0200

    test fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,11 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+
+    function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
     public function load(ObjectManager $manager)
     {
+
+
         // $product = new Product();
         // $manager->persist($product);
 
