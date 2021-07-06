ef9b84d4c894cdaa4869132fe962c6971c20b7a3
Louka- <simon.haudegond@gmail.com>
2021-07-06 10:07:03 +0200

    test deploiement prod

--- migrations/Version20210706080511.php
+++ migrations/Version20210706080511.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210608081509 extends AbstractMigration
+final class Version20210706080511 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,12 @@ final class Version20210608081509 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE plateau_de_joueur_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE plateau_de_joueur (id INT NOT NULL, race VARCHAR(45) NOT NULL, for_base INT DEFAULT NULL, def_base INT DEFAULT NULL, gold_base INT DEFAULT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE plateau_de_joueur_id_seq CASCADE');
-        $this->addSql('DROP TABLE plateau_de_joueur');
     }
 }
